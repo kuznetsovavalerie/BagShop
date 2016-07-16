6ef7f1042f054b84ba89e0e3d5134f2ef93fc5d0
kuznetsovavalerie <vkuznetsova@GFL-1546>
2016-07-16 19:00:37 +0300

    Add AutoMapper.

---
+++ BagShop/App_Code/AutoMapperConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BagShop.Common.Entities;
+using BagShop.Models;
+
+namespace BagShop.App_Code
+{
+    public static class AutoMapperConfiguration
+    {
+        private static IMapper _mapper;
+
+        public static IMapper Mapper
+        {
+            get
+            {
+                return _mapper;
+            }
+        }
+
+        public static void Init()
+        {
+            var enterpriseConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<BlogPost, BlogPostPreviewModel>();
+            });
+
+            _mapper = enterpriseConfig.CreateMapper();
+        }
+    }
+}
