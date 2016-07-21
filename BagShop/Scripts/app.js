6dac7331ded47b6e48b18eac546e2ff10a52a4f9
kuznetsovavalerie <vkuznetsova@GFL-1546>
2016-07-21 18:43:25 +0300

    Add angular libraries.

--- BagShop/Scripts/app.js
+++ BagShop/Scripts/app.js
@@ -1,1 +1,1 @@
-﻿
+﻿!function () { "use strict"; angular.module("app", ["ui.router", 'ui.router.state', 'ncy-angular-breadcrumb']) }();
--- BagShop/Scripts/app/controllers/productController.js
+++ BagShop/Scripts/app/controllers/productController.js
@@ -1,3 +1,23 @@
-﻿var productController = function ($scope) {
-    //$scope.
-}
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('shopController', shopController);
+
+    shopController.$inject = [
+                            '$scope',
+                            '$attrs',
+                            '$q',
+                            '$controller',
+                            '$state'];
+
+    var shopController = function ($scope) {
+        //$scope.
+        $scope.init = function (data) {
+
+        }
+
+
+    }
+});
