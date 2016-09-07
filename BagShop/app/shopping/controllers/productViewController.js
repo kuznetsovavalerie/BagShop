547cafccafb83c2ebac4ecf75b02587de14ff442
kuznetsovavalerie <vkuznetsova@GFL-1546>
2016-09-07 19:50:43 +0300

    Add shopping controllers.

---
+++ BagShop/app/shopping/controllers/productViewController.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('productController', productController);
+
+    productController.$inject = [
+                            '$scope',
+                            '$attrs',
+                            '$q',
+                            '$controller',
+                            '$state'];
+
+    function productController($scope,
+                            $attrs,
+                            $q,
+                            $controller,
+                            $state) {
+        $scope.state = $state;
+
+        function bindData(scope, data) {
+            for (var property in data) {
+                if (data.hasOwnProperty(property)) {
+                    scope[property] = data[property];
+                }
+            }
+        }
+
+        $scope.init = function (data) {
+            bindData($scope, data);
+
+            $scope.selectedColour = $scope.colours[0];
+        }
+
+        $scope.selectColour = function (colour) {
+            $scope.selectedColour = colour;
+        }
+
+        $scope.buyLink = function () {
+            return $scope.url + "?productId=${$scope.ID}&colourId=${$scope.selectedColour.ID}";            
+        }
+    }
+})();
---
+++ BagShop/app/shopping/controllers/shoppingController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('shoppingController', shoppingController);
+
+    shoppingController.$inject = [
+                            '$scope',
+                            '$attrs',
+                            '$q',
+                            '$controller',
+                            '$state'];
+
+    function shoppingController($scope,
+                            $attrs,
+                            $q,
+                            $controller,
+                            $state) {
+        $scope.state = $state;
+
+        $scope.init = function () {
+
+        }
+    }
+})();
