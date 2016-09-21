6633f8e2b82de3f8fefedb45bc723af43d0b38c4
Valeriia Kuznetsova <vkuznetsova@geeksforless.net>
2016-09-21 09:05:05 +0300

    Fix AutMapper config.

---
+++ BagShop/App_Code/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+
+namespace BagShop.App_Code
+{
+    public static class Configuration
+    {
+        public static string ProductsImageFolder {
+            get {
+                return ConfigurationManager.AppSettings["ProductsImageFolder"].ToString();
+            }
+        }
+
+        public static string ColoursImageFolder
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ColoursImageFolder"].ToString();
+            }
+        }
+
+        public static string TitleImageName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["TitleImageName"].ToString();
+            }
+        }
+    }
+}
