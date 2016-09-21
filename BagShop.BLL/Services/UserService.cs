0c741b58e73f04f3983d9751f3e30d3dc866cdd7
Valeriia Kuznetsova <vkuznetsova@geeksforless.net>
2016-09-21 09:01:00 +0300

    Add User service.

---
+++ BagShop.BLL/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using BagShop.Common.Entities;
+using BagShop.Common.Interfaces;
+using BagShop.Common.Interfaces.Services;
+using System;
+
+namespace BagShop.BLL.Services
+{
+    public class UserService : BaseService, IUserService
+    {
+        public UserService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        { }
+
+        public User Get(Guid id)
+        {
+            return unitOfWork.UserRepository.FindById(id);
+        }
+
+        public void Update(User user)
+        {
+            unitOfWork.UserRepository.Update(user);
+        }
+    }
+}
---
+++ BagShop.Common/Interfaces/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using BagShop.Common.Entities;
+using System;
+
+namespace BagShop.Common.Interfaces.Services
+{
+    public interface IUserService
+    {
+        User Get(Guid id);
+
+        void Update(User item);
+    }
+}
